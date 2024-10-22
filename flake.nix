c9511fdb0005aa9f5b6bf364dad51c7b42882310
Storm-77 <czechu600@gmail.com>
2024-10-23 00:15:33 +0200

    catalog layout

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Development environment for blob-royale game";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in
+  {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs 
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.npm
+        ];
+
+
+      };
+  };
+}
+
